ccc6551e567ecbf2c58ba8fe68cbbefea6a27b74
Reza Akmal <rezajakmal@gmail.com>
2025-11-30 01:11:13 +0700

    implement JWT blacklist to protect middleware

--- backend/src/middleware/auth.middleware.js
+++ backend/src/middleware/auth.middleware.js
@@ -1,17 +1,27 @@
 import jwt from "jsonwebtoken";
 import User from "../models/User.js";
+import BlacklistedToken from "../models/BlacklistedToken.js";
 
 export const protect = async (req, res, next) => {
   const authHeader = req.headers.authorization;
-
+  
   if (!authHeader)
     return res.status(401).json({ message: "No token provided" });
-
+  
   const token = authHeader.split(" ")[1];
-
+  
   try {
+    // Check the token
+    const isBlacklisted = await BlacklistedToken.findOne({ token });
+    if (isBlacklisted) {
+      return res.status(401).json({ message: "Token has been logged out" });
+    }
+    
+    // JWT verification
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
     req.user = await User.findById(decoded.id).select("-password");
+
     next();
   } catch (err) {
     res.status(401).json({ message: "Invalid token" });
