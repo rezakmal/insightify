26c1756013d708930ffb6ea3ccda0cffd4490c62
Reza Akmal <rezajakmal@gmail.com>
2025-12-07 23:44:24 +0700

    chore(app): register course, module, and quiz route handlers

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,13 +3,20 @@ import cors from "cors";
 import connectDB from "./config/db.js";
 
 import authRoutes from "./routes/auth.routes.js";
+import courseRoutes from "./routes/course.routes.js";
+import moduleRoutes from "./routes/module.routes.js";
+import quizRoutes from "./routes/quiz.routes.js";
 
 const app = express();
+
 app.use(cors());
 app.use(express.json());
 
 connectDB();
 
 app.use("/api/auth", authRoutes);
+app.use("/api/courses", courseRoutes);
+app.use("/api/modules", moduleRoutes);
+app.use("/api/quiz", quizRoutes);
 
 export default app;
